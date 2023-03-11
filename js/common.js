e5469ebe2d07ad4f4f64ad9f8913f5cc4e39487a
gmAnn00 <bu9458@gmail.com>
2023-03-11 20:20:47 +0900

    index.js common.js 분리 메뉴 트랜지션

---
+++ js/common.js
@@ -0,0 +1,34 @@
+setMenuWidth();
+changeMenu();
+
+function setMenuWidth() {
+  let windowWidth;
+  let wrapWidth;
+  windowWidth = $(window).width();
+  // wrapWidth = $(window).width();
+  $("#header").css("width", windowWidth);
+  $("#footer").css("width", wrapWidth);
+  $(window).resize(function (event) {
+    windowWidth = $(window).width();
+    console.log("windowWidth:", windowWidth);
+    $("#header").css("width", windowWidth);
+    // $("#footer").css("width", window);
+  });
+}
+
+function changeMenu() {
+  let scrollTop;
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+    if (scrollTop != 0) {
+      $("#header").css("background", "#fff");
+      $("#header").css("border", "1px solid #ccc");
+      $(".mainMenu > li > a").css("color", "#000");
+      $(".mainMenu > li:last-child a").css("color", "#fff");
+    } else {
+      $("#header").css("background", "#8a2be2");
+      $("#header").css("border", "none");
+      $(".mainMenu > li > a").css("color", "#fff");
+    }
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-setMenuWidth();
 document.querySelector(".imgBtn1").addEventListener("click", function () {
   document.querySelector(".bannerContainer").style.transform = "translate(0px)";
 });
@@ -10,18 +9,3 @@ document.querySelector(".imgBtn3").addEventListener("click", function () {
   document.querySelector(".bannerContainer").style.transform =
     "translate(-500px)";
 });
-
-function setMenuWidth() {
-  let windowWidth;
-  let wrapWidth;
-  windowWidth = $(window).width();
-  // wrapWidth = $(window).width();
-  $("#header").css("width", windowWidth);
-  $("#footer").css("width", wrapWidth);
-  $(window).resize(function (event) {
-    windowWidth = $(window).width();
-    console.log("windowWidth:", windowWidth);
-    $("#header").css("width", windowWidth);
-    // $("#footer").css("width", window);
-  });
-}

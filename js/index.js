1b7e34ca0fdb7cf1ec3bcc7c4803599b171d8863
gmAnn00 <bu9458@gmail.com>
2023-03-20 21:40:08 +0900

    index 애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -43,3 +43,102 @@ function moveSlider(index) {
   $("div.imgBtns > button").removeClass("active");
   $("div.imgBtns > button").eq(index).addClass("active");
 }
+
+// 복싱 애니메이션
+let boxing;
+$("#sportsContainer div.boxing").hover(
+  function () {
+    // console.log("hover");
+    moveboxing();
+    boxing = setInterval(function () {
+      moveboxing();
+    }, 3300);
+  },
+  function () {
+    // console.log("over");
+    clearInterval(boxing);
+    $(this).children("img").last().css({
+      left: 100,
+      top: 20,
+    });
+  }
+);
+
+function moveboxing() {
+  $("div.boxing").children("img").last().animate(
+    {
+      left: 80,
+      top: 10,
+    },
+    1000,
+    "easeOutElastic"
+  );
+  $("div.boxing").children("img").last().animate(
+    {
+      left: 100,
+      top: 20,
+    },
+    500
+  );
+  $("div.boxing").children("img").last().animate(
+    {
+      left: 120,
+      top: 10,
+    },
+    1000,
+    "easeOutElastic"
+  );
+  $("div.boxing").children("img").last().animate(
+    {
+      left: 100,
+      top: 20,
+    },
+    500
+  );
+}
+
+// 수영 애니메이션
+let swimming;
+$("#sportsContainer div.swimming").hover(
+  function () {
+    moveswimming();
+  },
+  function () {
+    clearInterval(swimming);
+    $(this).children("img").last().removeClass("animate");
+    $(this).children("img").last().css({
+      left: 70,
+      top: 115,
+    });
+  }
+);
+
+function moveswimming() {
+  $("div.swimming")
+    .children("img")
+    .last()
+    .animate(
+      {
+        left: 140,
+        top: 100,
+      },
+      "slow",
+      function () {
+        $("div.swimming").children("img").last().addClass("animate");
+      }
+    );
+
+  $("div.swimming")
+    .children("img")
+    .last()
+    .animate(
+      {
+        left: 70,
+        top: 115,
+      },
+      "slow",
+      function () {
+        $("div.swimming").children("img").last().removeClass("animate");
+      }
+    );
+}

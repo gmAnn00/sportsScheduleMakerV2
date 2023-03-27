4876ac0ac68256729d757ab014a496e22283f1c6
gmAnn00 <bu9458@gmail.com>
2023-03-27 21:40:16 +0900

    애니메이션 수정

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(window).scroll(function () {
   }
 });
 function showSportsImg() {
-  $("#sportsContainer div.visualImg")
+  $("div.visualImg")
     .first()
     .show("normal", function showNext() {
       $(this).next("div.visualImg").show("nomal", showNext);
@@ -49,15 +49,22 @@ function moveSlider(index) {
 
 // 복싱 애니메이션
 let boxing;
-$("#sportsContainer div.boxing").hover(
+let boxingPlaying = false;
+$("div.boxing").hover(
   function () {
-    // console.log("hover");
-    moveboxing();
-    boxing = setInterval(moveboxing, 3300);
+    boxingPlaying = true;
+    moveboxing($("div.boxing"));
+    boxing = setTimeout(function moveboxingRecursion() {
+      if (!boxingPlaying) {
+        return;
+      }
+      moveboxing($("div.boxing"));
+      setTimeout(moveboxingRecursion, 3300);
+    }, 3300);
   },
   function () {
-    // console.log("over");
-    clearInterval(boxing);
+    clearTimeout(boxing);
+    boxingPlaying = false;
     $(this).children("img").last().css({
       left: 100,
       top: 20,
@@ -65,8 +72,9 @@ $("#sportsContainer div.boxing").hover(
   }
 );
 
-function moveboxing() {
-  $("div.boxing").children("img").last().animate(
+function moveboxing(obj) {
+  let target = obj.children("img").last();
+  target.animate(
     {
       left: 80,
       top: 10,
@@ -74,14 +82,14 @@ function moveboxing() {
     1000,
     "easeOutElastic"
   );
-  $("div.boxing").children("img").last().animate(
+  target.animate(
     {
       left: 100,
       top: 20,
     },
     500
   );
-  $("div.boxing").children("img").last().animate(
+  target.animate(
     {
       left: 120,
       top: 10,
@@ -89,7 +97,7 @@ function moveboxing() {
     1000,
     "easeOutElastic"
   );
-  $("div.boxing").children("img").last().animate(
+  target.animate(
     {
       left: 100,
       top: 20,
@@ -100,49 +108,54 @@ function moveboxing() {
 
 // 수영 애니메이션
 let swimming;
-$("#sportsContainer div.swimming").hover(
+let swimmingPlaying = false;
+$("div.swimming").hover(
   function () {
-    moveswimming();
-    swimming = setInterval(moveswimming, 1400);
+    swimmingPlaying = true;
+    moveswimming($("div.swimming"));
+    swimming = setTimeout(function moveswimmingRecursion() {
+      if (!swimmingPlaying) {
+        return;
+      }
+      moveswimming($("div.swimming"));
+      setTimeout(moveswimmingRecursion, 1400);
+    });
   },
   function () {
-    clearInterval(swimming);
-    $(this).children("img").last().removeClass("animate");
-    $(this).children("img").last().css({
+    clearTimeout(swimming);
+    swimmingPlaying = false;
+    let target = $("div.swimming").children("img").last();
+    target.removeClass("swimmingAni");
+    target.css({
       left: 70,
       top: 115,
     });
   }
 );
 
-function moveswimming() {
-  $("div.swimming")
-    .children("img")
-    .last()
-    .animate(
-      {
-        left: 140,
-        top: 100,
-      },
-      "slow",
-      function () {
-        $("div.swimming").children("img").last().addClass("swimmingAni");
-      }
-    );
+function moveswimming(obj) {
+  let target = obj.children("img").last();
+  target.animate(
+    {
+      left: 140,
+      top: 100,
+    },
+    "slow",
+    function () {
+      target.addClass("swimmingAni");
+    }
+  );
 
-  $("div.swimming")
-    .children("img")
-    .last()
-    .animate(
-      {
-        left: 70,
-        top: 115,
-      },
-      "slow",
-      function () {
-        $("div.swimming").children("img").last().removeClass("swimmingAni");
-      }
-    );
+  target.animate(
+    {
+      left: 70,
+      top: 115,
+    },
+    "slow",
+    function () {
+      target.removeClass("swimmingAni");
+    }
+  );
 }
 
 // 클라이밍 애니메이션
@@ -150,22 +163,23 @@ let climbing;
 $("div.climbing").hover(
   function () {
     moveclimbing();
-    climbing = setInterval(moveclimbing, 4000);
+    climbing = setTimeout(function moveclimbingRecursion() {
+      moveclimbing();
+    });
   },
   function () {
     clearInterval(climbing);
-    $("div.climbing").children("img").eq(1).removeClass("climbingAni");
     $("div.climbing").children("img").eq(1).css({
       display: "inline-block",
       left: 85,
       top: 130,
     });
+    $("div.climbing").children("img").eq(1).show();
   }
 );
 
 function moveclimbing() {
   $("div.climbing").children("img").eq(1).show();
-  $("div.climbing").children("img").eq(1).removeClass("climbingAni");
   $("div.climbing").children("img").eq(1).css({
     left: 85,
     top: 130,
@@ -185,22 +199,21 @@ function moveclimbing() {
         $("div.climbing").children("img").eq(1).fadeOut(500);
         $("div.climbing").children("img").eq(2).fadeIn(500);
         $("div.climbing").children("img").eq(2).delay(1000).fadeOut(500);
+        $("div.climbing").children("img").eq(1).show();
       }
     );
 }
 
 // 필라테스 애니메이션
 let pilates;
-$("div.pilates")
-  .children("img")
-  .hover(
-    function () {
-      $("div.pilates").children("img").first().addClass("pilatesAni");
-    },
-    function () {
-      $("div.pilates").children("img").first().removeClass("pilatesAni");
-    }
-  );
+$("div.pilates").hover(
+  function () {
+    $(this).children("img").first().addClass("pilatesAni");
+  },
+  function () {
+    $(this).children("img").first().removeClass("pilatesAni");
+  }
+);
 
 // 자동 넘김 이미지 슬라이더
 $("#eventPanel").prepend($("img.eventImg").eq(-1).clone());
